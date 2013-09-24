5f260b43db070b59ada2594e527ddb273fcbb6d2
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-24 17:31:41 -0500

    add rcv for3 students, uses named routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,11 @@
 class StudentsController < ApplicationController
   def show_all
   end
+
+  def ami
+  end
+  def jae
+  end
+  def nick
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Connectedin::Application.routes.draw do
   root "students#show_all"
+  get "/students"  => "students#show_all"
+  get "/ami"       => "students#ami"  , as: 'ami'
+  get "/jae"       => "students#jae"  , as: 'jae'
+  get "/nick"      => "students#nick" , as: 'nick'
 end
